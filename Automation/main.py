2bf2e822d70daa74dba3f69894908148f16f872c
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-28 12:00:20 -0300

    Empty table to scroll through files

---
+++ Automation/main.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+path = "FilePath"
+files = os.listdir(path)
+print(files)
+
+consolidated_table = pd.DataFrame()
+
+for file_name in files:
+  "FilePath"
+  print(file_name)
+ 
